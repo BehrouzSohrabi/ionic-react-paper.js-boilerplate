4618f0fd7c7925a700ad405a663d6ac13bc34cfe
Behrouz Sohrabi <10177282+BehrouzSohrabi@users.noreply.github.com>
2021-12-25 10:57:17 +0330

    ionic app setup completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import "./styles.css";
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+import "./theme/variables.css";
+import "./theme/styles.css";
+
+import { Redirect, Route } from "react-router-dom";
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+
 import Canvas from "./Canvas";
+
+setupIonicReact();
 
 export default function App() {
   return (
-    <>
-      <Canvas />
-    </>
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Canvas />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { render } from "react-dom";
 
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
+
+serviceWorkerRegistration.register();
